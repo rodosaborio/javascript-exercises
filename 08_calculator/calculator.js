7e9a4153f61f528c760edbb454bd3bcc3477f1be
Rodolfo Saborio <rsaborio@hopper.com>
2023-12-27 14:01:51 -0600

    feat: complete calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = (a, b) => {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = (a, b) => {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = (array) => {
+	return array.reduce((acc, i) => acc + i, 0);
 };
 
-const multiply = function() {
-
+const multiply = (array) => {
+  return array.reduce((acc, i) => acc * i, 1);
 };
 
-const power = function() {
-	
+const power = (a, b) => {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = (a) => {
+  if (a === 0) {
+    return 1;
+  }
+  result = a;
+	for (let i = a - 1; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
